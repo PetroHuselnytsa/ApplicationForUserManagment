4e9d0266b713fcfe3e339f578216be4903a11b99
PetroHuselnytsa <petroguselnica@gmail.com>
2026-04-19 11:03:30 +0300

    Revert "SCRUM-323 Implement Json Web Token Auth System (With Skills)"

--- TestFirstProject/Models/DTOs/ApiErrorResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace TestFirstProject.Models.DTOs
-{
-    public class ApiErrorResponse
-    {
-        public string Message { get; set; } = null!;
-
-        public IEnumerable<string>? Errors { get; set; }
-    }
-}
